c0e186821b339feb83f17060a6f0a592dbba575c
Glitch (starter-react-template) <none>
2020-04-20 12:30:07 +0000

    📇🌱 Checkpoint ./src/components/Modal.js:15151860/594

---
+++ src/components/Modal.js
@@ -0,0 +1,2 @@
+// 1. Code ra như design https://www.figma.com/file/M2Lh7XaEKWnds9SeEuJ1l0/B%C3%A0i-t%E1%BA%ADp-CSS-cho-entry-test?node-id=87%3A102
+// 2. Thêm prop `content` thay vì hard-code text content trong Modal
